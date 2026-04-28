d719ff76e3aa525482e7f74538a2d9d018ff3b44
aleburrascano <aleburrascano123@gmail.com>
2026-04-28 12:56:03 -0400

    fix: convert Windows path to POSIX format for Git Bash (C:/... -> /c/...)

--- bin/vault-init.js
+++ bin/vault-init.js
@@ -3,6 +3,7 @@
 const { spawnSync } = require('child_process');
 const path = require('path');
 let script = path.resolve(__dirname, '..', 'vault-init.sh');
-if (process.platform === 'win32') script = script.replace(/\\/g, '/');
+if (process.platform === 'win32')
+  script = script.replace(/\\/g, '/').replace(/^([A-Za-z]):\//, (_, d) => `/${d.toLowerCase()}/`);
 const result = spawnSync('bash', [script, ...process.argv.slice(2)], { stdio: 'inherit' });
 process.exit(result.status || 0);
